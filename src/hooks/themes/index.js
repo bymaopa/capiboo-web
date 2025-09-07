f418bd8d39ac534abae71a09de7a8d2806a79164
🐀 <bymaopa@gmail.com>
2025-09-07 21:12:29 +0200

    feat: se agrega un input en header para la busqueda en la app

--- src/hooks/themes/index.js
+++ src/hooks/themes/index.js
@@ -1,22 +1,21 @@
-
 // @scripts/themes/index.js
 
 /* eslint-disable no-undef */
 
 const theme = (() => {
-  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-    return localStorage.getItem('theme');
-  }
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    return 'dark';
-  }
-  return 'light';
+	if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+		return localStorage.getItem('theme');
+	}
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		return 'dark';
+	}
+	return 'light';
 })();
 
 if (theme === 'dark') {
-  document.documentElement.classList.add('dark');
+	document.documentElement.classList.add('dark');
 } else {
-  document.documentElement.classList.remove('dark');
+	document.documentElement.classList.remove('dark');
 }
 
 window.localStorage.setItem('theme', theme);
