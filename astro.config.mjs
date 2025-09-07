949c1c4f3c8088fd53908ce1b562cbe27b456e0b
🐀 <bymaopa@gmail.com>
2025-09-07 19:58:10 +0200

    feat: se aplica mejoras de comportamiento en la navegacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
     svelte()
   ],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss({ config: './tailwind.config.mjs' })],
     resolve: {
       alias: {
         '@': '/src',
